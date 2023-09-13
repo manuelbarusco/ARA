e3b4637b0f2d4a77fcb0c6ec381e1fddfaa07436
manuelbarusco <barusco.manuel@gmail.com>
2023-09-13 11:32:49 +0200

    Fix

--- script/results_analysis/generate_scores.py
+++ script/results_analysis/generate_scores.py
@@ -1,6 +1,5 @@
 """
-This script will extract the scores needed for a given results analysis, so for example
-the ndcg@5 for all the runs of a given query. 
+This script will extract the scores needed for a paired T-Test of the runs
 """
 
 import os
@@ -11,10 +10,12 @@ import csv
 """
 This function will create the csv output scores file
 @param: output_scores_path, path to the output scores file
+@param: trec_eval, string with the trec_eval results
+@param: score, string with the score
 """
-def generateOutputCSV(output_scores_path: str):
+def generateOutputCSV(output_scores_path: str, trec_eval, score: str):
     output_scores_file = open(output_scores_path, "w")
-    fieldnames = ['query_id', "score"]
+    fieldnames = ['query_id', score]
     csv_writer = csv.writer(output_scores_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(fieldnames)
 
@@ -25,58 +26,150 @@ def generateOutputCSV(output_scores_path: str):
             break
 
         query_id = split[1]
-        ndcg = float(split[2])
 
-        row = [query_id, ndcg]
-        csv_writer.writerow(row) 
+        if query_id != "all":
+
+            ndcg = float(split[2])
+
+            row = [query_id, ndcg]
+            csv_writer.writerow(row) 
 
     output_scores_file.close()
+
+"""
+This function will create the csv output scores files for ACORDAR
+@param: run_path, directory path where all the runs are scored
+@param: no_empty, flag that indicates if we have to consider the qrels with empty datasets
+"""
+def generateAcordarScores(directory_run_path:str, no_empty:bool):
+    scores = ["ndcg_cut.5","ndcg_cut.10","map_cut.5","map_cut.10"]
+    configurations = ["[d]", "[m]", "[m+d]"]
+    similarities = ["BM25", "TF-IDF", "LMD", "FSDM"]
+
+    scores_directory = None
+
+    if no_empty:
+        scores_directory = directory_run_path +"/scores-noempty"
+    else:
+        scores_directory = directory_run_path +"/scores"
+
+
+    #create the directory for the scores
+    if not os.path.exists(scores_directory):
+        os.makedirs(scores_directory)
+
+    for configuration in configurations:
+        #create the directory
+        output_directory = scores_directory
+
+        if configuration == "[m]":
+            output_directory = scores_directory +"/metadata"
+        elif configuration == "[d]":
+            output_directory = scores_directory +"/data"
+        elif configuration == "[m+d]":
+            output_directory = scores_directory +"/full"
+        
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        for similarity in similarities:
+
+            run_file_path = directory_run_path + "/" + similarity + configuration + ".txt"
+
+            for score in scores:
+
+                output_scores_path = output_directory + "/" + similarity + score + ".csv"
+
+                qrels = None
+
+                if no_empty:
+                    qrels = qrels_noempty_path
+                else:
+                    qrels = qrels_path
+        
+                #launch the trec_eval command for generating the scores
+                trec_eval = subprocess.run(
+                        [trec_eval_path, '-c', '-q', '-m', score, qrels, run_file_path], stdout=subprocess.PIPE).stdout.decode('UTF-8')
+
+                #generate our scores file
+                generateOutputCSV(output_scores_path, trec_eval, score)
+        
+"""
+This function will create the csv output scores files for ACORDAR
+@param: run_path, directory path where all the runs are scored
+@param: no_empty, flag that indicates if we have to consider the qrels with empty datasets
+"""
+def generateARDFScores(directory_run_path:str, no_empty:bool):
+    scores = ["ndcg_cut.5","ndcg_cut.10","map_cut.5","map_cut.10"]
+    configurations = ["[d]", "[m]", "[m+d]"]
+    #configurations = ["[m]"]
+    similarities = ["BM25Boost", "TFIDFBoost", "LMDBoost", "FSDM"]
+
+    scores_directory = None
+
+    if no_empty:
+        scores_directory = directory_run_path +"/scores-noempty"
+    else:
+        scores_directory = directory_run_path +"/scores"
+
+    #create the directory for the scores
+    if not os.path.exists(scores_directory):
+        os.makedirs(scores_directory)
+
+    for configuration in configurations:
+        #create the directory
+        output_directory = scores_directory
+
+        if configuration == "[m]":
+            output_directory = scores_directory +"/metadata"
+        elif configuration == "[d]":
+            output_directory = scores_directory +"/data"
+        elif configuration == "[m+d]":
+            output_directory = scores_directory +"/full"
+        
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        for similarity in similarities:
+
+            run_file_path = directory_run_path + "/" + similarity + configuration + ".txt"
+
+            for score in scores:
+
+                output_scores_path = output_directory + "/" + similarity + score + ".csv"
+
+                qrels = None
+
+                if no_empty:
+                    qrels = qrels_noempty_path
+                else:
+                    qrels = qrels_path
+        
+                #launch the trec_eval command for generating the scores
+                trec_eval = subprocess.run(
+                        [trec_eval_path, '-c', '-q', '-m', score, qrels, run_file_path], stdout=subprocess.PIPE).stdout.decode('UTF-8')
+
+                #generate our scores file
+                generateOutputCSV(output_scores_path, trec_eval, score)
+        
 
 
 if __name__ == "__main__":
 
     dirname = os.path.dirname(__file__)
 
-    #param needed for retrieving the ACORDAR runs and my runs for the scores file production
-    run_id = "Stream_Jena_LightRDF_Deduplication"
-    configuration = "[d]"
-    similarity = "BM25Boost"
-    similarity_acordar = "BM25F"
-    score = "P.10"
-
     #trec eval executable
     trec_eval_path = "/home/manuel/Tesi/TREC/trec_eval/trec_eval"
 
     #input files
     qrels_path = os.path.join(dirname, f'../../files/qrels.txt')
-    run_file_path = os.path.join(dirname, f'../../files/run/ARDFS/{run_id}/{similarity}{configuration}.txt')
-    acordar_run_path = os.path.join(dirname, f'../../files/run/ACORDAR/{similarity_acordar}{configuration}.txt')
+    qrels_noempty_path = os.path.join(dirname, f'../../output/qrels_without_empty.txt')
+    
+    #flag for considering qrels with empty datasets or not
+    no_empty = True
 
-    #output file
-    output_scores_directory = os.path.join(dirname, f'../../files/run/ARDFS/{run_id}/scores')
-    output_scores_path = os.path.join(dirname, f'../../files/run/ARDFS/{run_id}/scores/{similarity}{configuration}.csv')
-    acordar_scores_path = os.path.join(dirname, f'../../files/run/ACORDAR/scores/{similarity_acordar}{configuration}.csv')
-
-    #create directory for our scores if it does not exists 
-    if not os.path.exists(output_scores_directory):
-        os.makedirs(output_scores_directory)
-
-    #launch the trec_eval command for generating our scores
-    trec_eval = subprocess.run(
-            [trec_eval_path, '-q', '-m', score, qrels_path, run_file_path], stdout=subprocess.PIPE).stdout.decode('UTF-8')
-
-    #generate our scores file
-    generateOutputCSV(output_scores_path)
-
-    #create directory for ACORDAR scores if it does not exists 
-    if not os.path.exists(output_scores_directory):
-        os.makedirs(output_scores_directory)
-
-    #launch the trec_eval command for generating ACORDAR scores
-    trec_eval = subprocess.run(
-            [trec_eval_path, '-q', '-m', score, qrels_path, acordar_run_path], stdout=subprocess.PIPE).stdout.decode('UTF-8')
-
-    #generate ACORDAR scores file
-    generateOutputCSV(acordar_scores_path)
+    #generateAcordarScores(os.path.join(dirname, f'../../files/run/ACORDAR'), no_empty)
+    
+    generateARDFScores(os.path.join(dirname, f'../../files/run/ARDFS/Stream_Deduplication'), no_empty)
 
 
---
+++ script/results_analysis/paired_t_test.py
@@ -0,0 +1,50 @@
+import scipy.stats as stats
+import os
+import pandas as pd
+
+dirname = os.path.dirname(__file__)
+
+configuration = "[m]"
+run = "Basic_Parsing"
+similarities_acordar = ["TF-IDF", "BM25", "FSDM", "LMD"]
+similarities = ["TFIDFBoost", "BM25Boost", "FSDM", "LMDBoost"]
+scores = ["ndcg_cut.5"]
+no_empty = False
+
+acordar_scores_directory_path = None
+ardfs_scores_directory_path = None
+
+if no_empty:
+    acordar_scores_directory_path = os.path.join(dirname, f'../../files/run/ACORDAR/scores-noempty')
+    ardfs_scores_directory_path = os.path.join(dirname, f'../../files/run/ARDFS/{run}/scores-noempty')
+else:
+    acordar_scores_directory_path = os.path.join(dirname, f'../../files/run/ACORDAR/scores')
+    ardfs_scores_directory_path = os.path.join(dirname, f'../../files/run/ARDFS/{run}/scores')
+
+if configuration == "[m]":
+    acordar_scores_directory_path += "/metadata"
+    ardfs_scores_directory_path += "/metadata"
+elif configuration == "[d]":
+    acordar_scores_directory_path += "/data"
+    ardfs_scores_directory_path += "/data"
+elif configuration == "[m+d]":
+    acordar_scores_directory_path += "/full"
+    ardfs_scores_directory_path += "/full"
+
+for i in range(len(similarities_acordar)):
+    for score in scores:
+        score_path_acordar = acordar_scores_directory_path + "/" + similarities_acordar[i] + score + ".csv"
+        score_path_ardfs = ardfs_scores_directory_path + "/" + similarities[i] + score + ".csv"
+
+        #open the two dataframes for the csv files
+        df_acordar = pd.read_csv(score_path_acordar, sep = ',')
+        df_ardfs = pd.read_csv(score_path_ardfs, sep = ',')
+
+        acordar_scores = list(df_acordar[score])
+        ardfs_scores = list(df_ardfs[score])
+
+        #print the results
+        print(f"Similarity: {similarities_acordar[i]} Score: {score}")
+        print(f"Mean Value ACORDAR: {sum(acordar_scores) / len(acordar_scores)}")
+        print(f"Mean Value ARDFS: {sum(ardfs_scores) / len(ardfs_scores)}")
+        print(stats.ttest_rel(acordar_scores, ardfs_scores).pvalue)
